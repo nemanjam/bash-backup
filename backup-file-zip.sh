28ed35222f2f95142e9a95a3c4de47a91f674124
nemanjam <nemanja.mitic.elfak@hotmail.com>
2026-01-20 13:04:43 +0100

    backup multiple dirs in progress

--- backup-file-zip.sh
+++ backup-file-zip.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-
 # ---------- Configuration ----------
 
-SRC_CODE="/var/www/html"
+declare -A SRC_CODE_DIRS=(
+    ["project"]="/var/www/html"
+    ["blog"]="/var/www/blog"
+    ["docs"]="/var/www/docs"
+)
+
 LOCAL_BACKUP_DIR="/home/user/backup"
-PROJECT_NAME="project"
 
 BACKUP_RETENTION_DAILY=3
 BACKUP_RETENTION_WEEKLY=2
@@ -17,37 +20,46 @@ BACKUP_DAILY=$(( BACKUP_RETENTION_DAILY > 0 ))
 BACKUP_WEEKLY=$(( BACKUP_RETENTION_WEEKLY > 0 ))
 BACKUP_MONTHLY=$(( BACKUP_RETENTION_MONTHLY > 0 ))
 
-MONTH=`date +%d`
-DAY_WEEK=`date +%u`
+# Current day and weekday
+MONTH=$(date +%d)
+DAY_WEEK=$(date +%u)
 
 if [[ ( $MONTH -eq 1 ) && ( $BACKUP_MONTHLY == true ) ]];
         then
-        FREQ='monthly'
+    FREQ='monthly'
 elif [[ ( $DAY_WEEK -eq 7 ) && ( $BACKUP_WEEKLY == true ) ]];
         then
-        FREQ='weekly'
+    FREQ='weekly'
 elif [[ ( $DAY_WEEK -lt 7 ) && ( $BACKUP_DAILY == true ) ]];
         then
-        FREQ='daily'
+    FREQ='daily'
 fi
 
-DATE=$FREQ-`date +"%Y%m%d"`
+DATE=$FREQ-$(date +"%Y%m%d")
 
-function local_only
-{
-	zip -r $LOCAL_BACKUP_DIR/$PROJECT_NAME-htdocs-$DATE.zip $SRC_CODE -x *wp-content/uploads*
-	cd $LOCAL_BACKUP_DIR/
-	ls -t | grep $PROJECT_NAME | grep htdocs | grep daily | sed -e 1,"$BACKUP_RETENTION_DAILY"d | xargs -d '\n' rm -R > /dev/null 2>&1
-	ls -t | grep $PROJECT_NAME | grep htdocs | grep weekly | sed -e 1,"$BACKUP_RETENTION_WEEKLY"d | xargs -d '\n' rm -R > /dev/null 2>&1
-	ls -t | grep $PROJECT_NAME | grep htdocs | grep monthly | sed -e 1,"$BACKUP_RETENTION_MONTHLY"d | xargs -d '\n' rm -R > /dev/null 2>&1
+function local_only {
+    for SRC_CODE_DIR in "${!SRC_CODE_DIRS[@]}"; do
+        SRC_CODE_DIR_PATH="${SRC_CODE_DIRS[$SRC_CODE_DIR]}"
+
+        # Zip the source directory
+        zip -r "$LOCAL_BACKUP_DIR/$SRC_CODE_DIR-htdocs-$DATE.zip" "$SRC_CODE_DIR_PATH" -x "*wp-content/uploads*"
+
+        # Move to backup directory
+        cd "$LOCAL_BACKUP_DIR/" || return 1
+
+        # Prune old backups based on retention
+        ls -t | grep "$SRC_CODE_DIR" | grep htdocs | grep daily | sed -e 1,"$BACKUP_RETENTION_DAILY"d | xargs -d '\n' rm -R > /dev/null 2>&1
+        ls -t | grep "$SRC_CODE_DIR" | grep htdocs | grep weekly | sed -e 1,"$BACKUP_RETENTION_WEEKLY"d | xargs -d '\n' rm -R > /dev/null 2>&1
+        ls -t | grep "$SRC_CODE_DIR" | grep htdocs | grep monthly | sed -e 1,"$BACKUP_RETENTION_MONTHLY"d | xargs -d '\n' rm -R > /dev/null 2>&1
+    done
 }
 
 if [[ ( $BACKUP_DAILY == true ) && ( ! -z "$BACKUP_RETENTION_DAILY" ) && ( $BACKUP_RETENTION_DAILY -ne 0 ) && ( $FREQ == daily ) ]]; then
-	local_only
+    local_only
 fi
 if [[ ( $BACKUP_WEEKLY == true ) && ( ! -z "$BACKUP_RETENTION_WEEKLY" ) && ( $BACKUP_RETENTION_WEEKLY -ne 0 ) && ( $FREQ == weekly ) ]]; then
-	local_only
+    local_only
 fi
 if [[ ( $BACKUP_MONTHLY == true ) && ( ! -z "$BACKUP_RETENTION_MONTHLY" ) && ( $BACKUP_RETENTION_MONTHLY -ne 0 ) && ( $FREQ == monthly ) ]]; then
-	local_only
+    local_only
 fi
